ddc2fc3536719619b5dcc6fd3b315068be057663
IGorkemOzturk <57742487+IGorkemOzturk@users.noreply.github.com>
2021-02-16 21:47:41 +0300

    Repositories

--- BellaBlog/Controllers/HomeController.cs
+++ BellaBlog/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BellaBlog.Models;
+using BellaBlog.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,18 @@ namespace BellaBlog.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        BlogRepository br = new BlogRepository();
+        AdminRepository ar = new AdminRepository();
+        Context c = new Context();
+        public IActionResult Index(int id)
         {
-            return View();
+            
+            return View(br.TList("Admin"));
+        }
+        public IActionResult BlogDetay(int id)
+        {
+            var detay = c.Blogs.Where(x =>  x.BlogId==id).ToList();
+            return View(detay);
         }
     }
 }
---
+++ BellaBlog/Repositories/FoodRepository.cs
@@ -0,0 +1,12 @@
+﻿using BellaBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BellaBlog.Repositories
+{
+    public class FoodRepository : GenericRepository<Food>
+    {
+    }
+}
